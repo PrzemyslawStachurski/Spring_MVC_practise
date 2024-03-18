9ab6846979b1e9a1eda8b36a94cb76ece201d366
Przemysław Stachurski <przemekstachurski737@gmail.com>
2024-03-18 15:22:28 +0100

    Initial commit

---
+++ Thymeleaf-helloworld/src/main/java/com/example/MVClearning/MvClearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.MVClearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvClearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvClearningApplication.class, args);
+	}
+
+}
---
+++ Thymeleaf-helloworld/src/test/java/com/example/MVClearning/MvClearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MVClearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvClearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
