f2b6f941e6f394af8027ebdc9ddb98bda097f9c1
Przemysław Stachurski <przemekstachurski737@gmail.com>
2024-03-22 20:57:48 +0100

    implemented adding data to model

--- Thymeleaf-helloworld/src/main/java/com/example/MVClearning/Controller/HelloWorldController.java
+++ Thymeleaf-helloworld/src/main/java/com/example/MVClearning/Controller/HelloWorldController.java
@@ -1,18 +1,33 @@
 package com.example.MVClearning.Controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloWorldController {
 
     @GetMapping("showForm")
-    public String showForm(){
-        return"helloworld-form";
+    public String showForm() {
+        return "helloworld-form";
     }
 
-    @GetMapping("processForm")
-    public String processForm(){
-        return"helloworld";
+    @RequestMapping("processForm")
+    public String processForm() {
+        return "helloworld";
+    }
+
+    @RequestMapping("processFormVersionTwo")
+    public String shoutName(HttpServletRequest request, Model model) {
+
+        String name = request.getParameter("studentName");
+
+        name = "hello " +name.toUpperCase();
+
+        model.addAttribute("message", name);
+
+        return "helloworld";
     }
 }
