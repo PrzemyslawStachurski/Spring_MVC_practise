d549eb4c6070acf25b8323a09fd012f4448e555c
przemek <przemekstachurski737@gmail.com>
2024-03-30 11:19:04 +0100

    added new project focusing on validating data

---
+++ learningValidation/src/main/java/com/example/learningValidation/LearningValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningValidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningValidationApplication.class, args);
+	}
+
+}
