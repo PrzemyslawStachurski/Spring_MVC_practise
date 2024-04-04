4fd4eaf9102153406362bb4dfa1f616874efa12c
Przemysław Stachurski <przemekstachurski737@gmail.com>
2024-04-04 14:37:44 +0200

    Added validation for fields requiring numbers

--- learningValidation/src/main/java/com/example/learningValidation/Model/Customer.java
+++ learningValidation/src/main/java/com/example/learningValidation/Model/Customer.java
@@ -1,15 +1,22 @@
 package com.example.learningValidation.Model;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
     private String firstName;
 
     @NotNull(message = "is required")
-    @Size(min=1,message = "is required")
+    @Size(min = 1, message = "is required")
     private String lastName;
+
+    @NotNull(message = "is required")
+    @Min(value = 0, message = "must be greater than or equal to 0")
+    @Max(value = 10, message = "must be lesser than or equal to 10")
+    private Integer freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message= "only 5 characters allowed")
+    private String postalCode;
 
 
     public String getFirstName() {
@@ -28,4 +35,19 @@ public class Customer {
         this.lastName = lastName;
     }
 
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 }
