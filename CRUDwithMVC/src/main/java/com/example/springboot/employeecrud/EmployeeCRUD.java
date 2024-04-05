96e6b662aaf13dfa8f6cc117f0fb1cd4a1e3cfb4
Przemysław Stachurski <przemekstachurski737@gmail.com>
2024-04-05 19:00:29 +0200

    implemented GET function and created a basic html view

--- CRUDwithMVC/src/main/java/com/example/springboot/employeecrud/EmployeeCRUD.java
+++ CRUDwithMVC/src/main/java/com/example/springboot/employeecrud/EmployeeCRUD.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CruddemoApplication {
+public class EmployeeCRUD {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CruddemoApplication.class, args);
+		SpringApplication.run(com.luv2code.springboot.cruddemo.EmployeeCRUD.class, args);
 	}
 
 }
