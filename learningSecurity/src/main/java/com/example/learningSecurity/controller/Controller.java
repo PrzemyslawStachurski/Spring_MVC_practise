c7260091374be7034b144132b6dea4d669b66132
Przemysław Stachurski <przemekstachurski737@gmail.com>
2024-04-10 17:24:41 +0200

    implemented mandatory login and redirecting to custom login page

--- learningSecurity/src/main/java/com/example/learningSecurity/controller/Controller.java
+++ learningSecurity/src/main/java/com/example/learningSecurity/controller/Controller.java
@@ -2,6 +2,8 @@ package com.example.learningSecurity.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+
 @org.springframework.stereotype.Controller
 public class Controller {
 
