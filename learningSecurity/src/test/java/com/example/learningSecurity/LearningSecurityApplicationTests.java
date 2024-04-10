e314087a75992989960e390a539658e6a7edcee6
Przemysław Stachurski <przemekstachurski737@gmail.com>
2024-04-10 16:13:49 +0200

    created new project to familiarize myself with Spring security in MVC projects

---
+++ learningSecurity/src/test/java/com/example/learningSecurity/LearningSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
