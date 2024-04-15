735531f6cdaf25627528f5f28d8af4d1a574a326
Przemysław Stachurski <przemekstachurski737@gmail.com>
2024-04-15 19:42:05 +0200

    Added custom eroor 403 page and restricted pages based on roles

--- learningSecurity/src/main/java/com/example/learningSecurity/controller/Controller.java
+++ learningSecurity/src/main/java/com/example/learningSecurity/controller/Controller.java
@@ -11,4 +11,14 @@ public class Controller {
     public String showHome(){
         return "home";
     }
+
+    @GetMapping("/leaders")
+    public String showLeaders(){
+        return "leaders";
+    }
+
+    @GetMapping("/systems")
+    public String showSystems(){
+        return "systems";
+    }
 }
